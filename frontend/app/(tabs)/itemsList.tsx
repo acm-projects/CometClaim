0fc9b46dd5639487b14020f8adfe44077fae0849
mohammadmehrab <108484416+mohammadmehrab@users.noreply.github.com>
2025-03-23 15:44:47 -0500

    Changed itemsList.tsx

--- frontend/app/(tabs)/itemsList.tsx
+++ frontend/app/(tabs)/itemsList.tsx
@@ -1,63 +1,86 @@
 import { useEffect, useState } from "react";
-import { Button, ScrollView, Text, View, Image} from "react-native";
+import { Button, ScrollView, Text, View, Image } from "react-native";
 import Constants from "expo-constants";
 
 interface Item {
-    item_id: string;
-    category: string;
-    date_reported: string;
-    description: string;
-    location: string;
-    reporter_id: string;
-    status: string;
-    image_url?: string;
+  item_id: string;
+  category: string;
+  date_reported: string;
+  description: string;
+  location: string;
+  reporter_id: string;
+  status: string;
+  image_url?: string;
 }
 
-const apiUrl = Constants.expoConfig?.extra?.apiUrl
+const apiUrl = Constants.expoConfig?.extra?.apiUrl;
 
 export default function ItemsListPage() {
-    const [items, setItems] = useState<Item[]>([])
-    useEffect(() => {
-        const getItems = async () => {
-            const res = await fetch(`${apiUrl}/items`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-            const data = await res.json()
-            setItems(JSON.parse(data.body))
-            console.log(data)
-        }
-        getItems()
-    }, [])
+  const [items, setItems] = useState<Item[]>([]);
+  useEffect(() => {
+    const getItems = async () => {
+      const res = await fetch(`${apiUrl}/items`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      setItems(JSON.parse(data.body));
+      console.log(data);
+    };
+    getItems();
+  }, []);
 
-    return (
+  return (
     <ScrollView>
-        <View style={{height: 100}}/>
-        {(items && items.length > 0) ? items.map((item : Item) => (
-            <View key={item.item_id} style={{ marginBottom: 10 }}>
-                {item.image_url && (
-                    <Image 
-                        source={{uri: item.image_url}} 
-                        style={{ width: 200, height: 200, borderRadius: 10, marginBottom: 10, borderColor: "red", borderWidth: 10 }}
-                        alt="TESTING"
-                    />
-                )}
-                
-                {
-                    Object.keys(item).map((key : any) => {
-                        if(key == "image_url") return null;
-                        return (<View key={key}>
-                            <Text style={{ fontWeight: 'bold', marginBottom: 5, color: '#fff' }}>{key.replace('_', ' ').toUpperCase() + ": " + item[key as keyof Item]}</Text>
-                        </View>)
-                    })
-                }
-            </View>
-        )) : <Text style={{ color: "red" }}>No items found</Text>
-        }
-        <Button title="Thing" onPress={()=>console.log(items)}></Button>
-        <View style={{height: 100}}/>
+      <View style={{ height: 100 }} />
+      {items && items.length > 0 ? (
+        items.map((item: Item) => (
+          <View
+            key={item.item_id}
+            style={{ marginBottom: 10, backgroundColor: "black", flex: 1 }}
+          >
+            {item.image_url && (
+              <Image
+                source={{ uri: item.image_url }}
+                style={{
+                  width: 200,
+                  height: 200,
+                  borderRadius: 10,
+                  marginBottom: 10,
+                  borderColor: "red",
+                  borderWidth: 10,
+                }}
+                alt="TESTING"
+              />
+            )}
+
+            {Object.keys(item).map((key: any) => {
+              if (key == "image_url") return null;
+              return (
+                <View key={key}>
+                  <Text
+                    style={{
+                      fontWeight: "bold",
+                      marginBottom: 5,
+                      color: "#fff",
+                    }}
+                  >
+                    {key.replace("_", " ").toUpperCase() +
+                      ": " +
+                      item[key as keyof Item]}
+                  </Text>
+                </View>
+              );
+            })}
+          </View>
+        ))
+      ) : (
+        <Text style={{ color: "red" }}>No items found</Text>
+      )}
+      <Button title="Thing" onPress={() => console.log(items)}></Button>
+      <View style={{ height: 100 }} />
     </ScrollView>
-    );
+  );
 }
